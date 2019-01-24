630cca678f17fe0258a3e1c307533719ced44168
Ergoold <eran1gold@gmail.com>
2019-01-24 11:39:27 +0200

    created class slider

---
+++ src/guishortcuts/consrainables/ConstrainedSlider.kt
@@ -0,0 +1,16 @@
+package guishortcuts.consrainables
+
+import java.awt.GridBagConstraints
+import javax.swing.*
+
+class ConstrainedSlider(min: Int, max: Int): JSlider(min, max), Constrainable {
+    override val GBC: GridBagConstraints = GridBagConstraints()
+
+    infix fun to(frame: JFrame) {
+        frame.add(this, GBC)
+    }
+}
+
+infix fun Int.add(slider: ConstrainedSlider): ConstrainedSlider {
+    return slider
+}
