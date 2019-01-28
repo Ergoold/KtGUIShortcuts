d6330c37d41b67560486d0a666c66ad8da8f7a98
Ergoold <eran1gold@gmail.com>
2019-01-28 10:49:30 +0200

    added does function & get selected function to ConstrainedSpinner

--- src/guishortcuts/consrainables/ConstrainedSpinner.kt
+++ src/guishortcuts/consrainables/ConstrainedSpinner.kt
@@ -9,6 +9,17 @@ class ConstrainedSpinner(text: Array<String>): JSpinner(text.toSpinnerModel()), 
     infix fun to(frame: JFrame) {
         frame.add(this, GBC)
     }
+
+    infix fun does(func: () -> Unit) {
+        addChangeListener {
+            func()
+        }
+    }
+
+    infix fun selected(kind: Int): String {
+        if(kind != item) throw Exception("Use 'item' when attempting to get selected list item")
+        return (model as SpinnerListModel).value.toString()
+    }
 }
 
 infix fun Int.add(spinner: ConstrainedSpinner): ConstrainedSpinner {
