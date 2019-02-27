c048a82eee56cc4d73e7c52c1f76f37b49343ab5
Ergoold <34137153+Ergoold@users.noreply.github.com>
2019-02-27 10:36:01 +0200

    added javadoc to ConstrainedButton

--- src/guishortcuts/consrainables/ConstrainedButton.kt
+++ src/guishortcuts/consrainables/ConstrainedButton.kt
@@ -4,27 +4,48 @@ import guishortcuts.make
 import java.awt.GridBagConstraints
 import javax.swing.*
 
+/**
+ * A button with GridBagConstraints.
+ */
 class ConstrainedButton(text: String): JButton(text), Constrainable {
+    /**
+     * This button's GridBagConstrains.
+     */
     override val GBC: GridBagConstraints = GridBagConstraints()
-
+    
+    /**
+     * Add this button to a frame.
+     */
     infix fun to(frame: JFrame) {
         frame.add(this, GBC)
     }
-
+    
+    /**
+     * Add an action listener to this button.
+     */
     infix fun does(func: () -> Unit) {
         addActionListener {func()}
     }
 }
 
+/**
+ * This function is purely for cosmetic purposes.
+ */
 infix fun Int.add(button: ConstrainedButton): ConstrainedButton {
     return button
 }
 
+/**
+ * Enable the button. Int must equal to the variable make.
+ */
 infix fun Int.enabled(button: ConstrainedButton) {
     if(this != make) throw Exception("Use 'make' when attempting to set component isEnabled")
     button.isEnabled = true
 }
 
+/**
+ * Disable the button. Int must equal to the variable make.
+ */
 infix fun Int.disabled(button: ConstrainedButton) {
     if(this != make) throw Exception("Use 'make' when attempting to set component isEnabled")
     button.isEnabled = false
